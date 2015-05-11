c22101631f54aeccb09618e12ec687fa02665fb9
lukesmmr <lukas@goodthingseverywhere.com>
2015-05-11 18:16:45 -0300

    basic history API functionality, embed flash files with SWFObject 2.3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-/*-------------------------------------------------------*\
-    Modernizr
-/*-------------------------------------------------------*/
-
-if(!Modernizr.svg) {
-
-    $('img[src*="svg"]').attr('src', function() {
-        return $(this).attr('src').replace('.svg', '.png');
-    });
-
-}
-
 
 /*-------------------------------------------------------*\
     Main
 /*-------------------------------------------------------*/
+
+$(document).ready(function(){
+
+	if (Modernizr.history) {
+
+	    console.log('yep, found the history API');
+
+	    $('nav a').on('click', function (e){
+	    	e.preventDefault();
+	    	//var slug = $(this).data('slug');
+	    	//var title = $(this).text();
+	    	var url = $(this).attr('href');
+	    	loadContent(url);
+	    });
+
+		window.addEventListener("popstate", function() {
+			var lastState = location.pathname;
+		    loadContent(lastState);
+		});
+	}
+});
+
+function loadContent(url) {
+	console.log('load content  ' + url)
+	$.get(url, function (data) {
+	    data = $(data).find('.main').html();
+	    $(".main").empty().append(data);
+	    history.pushState(null, null, url);
+	});
+}
+
+// flash business
+var params = {wmode: "transparent",scale: "noscale", quality: "low"}
+var attributes = {id: "rocket"}
+var rocket = document.getElementById("rocket");
+swfobject.embedSWF("flash/preloader.swf", rocket, "100%", "100%", 10, "", "", params, attributes);
