0ab2cd799a9c12c21133fe485ba186ec1a79242a
Lukas <lukas@goodthingseverywhere.com>
2015-09-08 01:27:35 -0300

    full rewrite using IIFE function, updating page title correctly and fixing onpopstate event / history sates. Rocket still needs custom event slugs to trigger events...

--- js/main.js
+++ js/main.js
@@ -1,51 +1,110 @@
-$(document).ready(function(){
+(function(){
+  'use strict';
 
-	if (Modernizr.flash) {
-		embedRocket();
-	}
+  // only run ajax if history api is supported
+  if (Modernizr.history) {
 
-	if (Modernizr.history) {
+    function loadContent(page) {
 
-	    console.log('yep, found the history API');
+      // update title 
+      document.title = page.title;
 
-	    $('nav a').on('click', function (e){
-	    	e.preventDefault();
-	    	//var slug = $(this).data('slug');
-	    	//var title = $(this).text();
-	    	var url = $(this).attr('href');
-	    	loadContent(url);
-	    });
+      // update content
+      $('.entry-content').html(page.content);
 
-	    // browser back - not working properly yet
-		window.addEventListener("popstate", function(e) {
-			var lastState = location.pathname;
-		    loadContent(lastState);
-		    talkToRocket(lastState.substring(0, lastState.length - 5).replace("/", ""))
-		});
-	}
+    }
 
-	$('nav a').on('click', function(){
-		talkToRocket( $(this).data('slug') );
-	});	
+    function pageState($content) {
 
-});
+      var page = {
+        content : $content.find('.entry-content').html(),
+        title : $content.filter('title').text()
+      };
 
-function talkToRocket(value) {
-	SWFAddress.setValue( value );
-}
+      return page;
 
-function loadContent(url) {
-	console.log('load content  ' + url)
-	$.get(url, function (data) {
-	    data = $(data).find('.main').html();
-	    $(".main").empty().append(data);
-	    history.pushState(null, null, url);
-	});
-}
+    }
 
-function embedRocket() {
-	var params = {wmode: "transparent",scale: "noscale", quality: "low"}
-	var attributes = {id: "rocket"}
-	var rocket = document.getElementById("rocket");
-	swfobject.embedSWF("flash/preloader.swf", rocket, "100%", "100%", 10, "", "", params, attributes);	
-}
+    $('nav a').on('click', function (event){
+      
+      event.preventDefault();
+      var url = $(this).attr('href');
+      console.log(url);
+
+      // request new page through ajax
+      var request = $.ajax(url);
+
+      // what to do with ajax success
+      request.done(function (data) {
+
+          // create state object
+          var page = pageState($(data));
+
+          // change the page content
+          loadContent(page);
+
+          // push the state into history
+          history.pushState(page, page.title, event.target.href);
+
+      });
+
+      // what to do if ajax fails
+      request.fail(function () {
+
+          // revert to normal navigation
+          document.location = event.target.href;
+
+      });
+
+      //console.log(url.substring(0, url.length - 5).replace("/", ""));
+      // use data object to set custom event value
+      talkToRocket(url.substring(0, url.length - 5).replace("/", ""))
+
+    });
+
+    // handle forward/back buttons
+    window.onpopstate = function(event) {
+
+        // guard against popstate event on chrome init
+        if (event.state) {
+            // get the state and change the page content
+            loadContent(event.state, true);
+        }
+        
+    };
+
+
+    function talkToRocket(value) {
+
+      SWFAddress.setValue( value );
+
+    }
+
+  }
+
+
+  // embed rocket flash if flash exists
+  if (Modernizr.flash) {
+    embedRocket();
+  }
+
+  function embedRocket() {
+
+    // set parameter objects & ID
+    var params = {
+      wmode: "transparent",
+      scale: "noscale", 
+      quality: "low"
+    }
+    var attributes = {
+      id: "rocket"
+    }
+    var rocket = document.getElementById("rocket");
+    
+    // use SWFObject library to embed swf
+    swfobject.embedSWF("flash/preloader.swf", 
+      rocket, "100%", "100%", 10, "", "", params, attributes);  
+
+  }
+
+}());
